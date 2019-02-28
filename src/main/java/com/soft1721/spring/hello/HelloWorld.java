999090703147ca96af3fbe31d31415f10057a47f
wangliuniu <3374465619@qq.com>
2019-02-28 11:07:10 +0800

    helloword project first file

---
+++ src/main/java/com/soft1721/spring/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.soft1721.spring.hello;
+
+/**
+ * Created by ASUS on 2019/2/25.
+ */
+public class HelloWorld {
+     public String getHello(){
+         return "Hello world";
+     }
+}
